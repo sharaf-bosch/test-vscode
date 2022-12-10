8e4e985a4c808fb36c86b1a955725b9d0e5063ee
sharaf-bosch <p.sharafudheen@in.bosch.com>
2022-12-10 13:25:12 +0530

    area and volume code added

---
+++ Python - Anaconda/Rectangle area.py
@@ -0,0 +1,4 @@
+l = float(input("Enter the length: "))
+w = float(input("Enter the width: "))
+area = l*w
+print("Area of the rectangle is ", area)
---
+++ Python - Anaconda/Volume of cube.py
@@ -0,0 +1,5 @@
+l = float(input("Enter the length (in cms): "))
+w = float(input("Enter the width (in cms): "))
+h = float(input("Enter the height (in cms): "))
+v = l*w*h
+print("Area of the rectangle is ", v, " sq. cms")
