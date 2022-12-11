33f42c71f40e4f5c1e1148be6ff4a7aa5c067cb5
sharaf-bosch <p.sharafudheen@in.bosch.com>
2022-12-11 16:26:24 +0530

    edited

--- Outlook meeting/test4.py
+++ Outlook meeting/test4.py
@@ -7,24 +7,29 @@ word_path = r'C:\Users\HPF1COB\Documents\Anaconda\VS code\Outlook meeting\output
 word = win32.gencache.EnsureDispatch('Word.Application')
 word.Visible = False
 word.DisplayAlerts = False
-
-excel_path = r'C:\Users\HPF1COB\Documents\Anaconda\VS code\Outlook meeting\input.xlsx'
-excel = win32.gencache.EnsureDispatch('Excel.Application')
-excel.Visible = False
-excel.DisplayAlerts = False
-wb = excel.Workbooks.Open(excel_path)
-ws = wb.Worksheets(1)
-ws.Range("A1:L7").Copy()
-wb.Close()
-
+doc = word.Documents.Open(word_path)
+doc.Content.Copy()
+doc.Close()
 
 outlook = win32.Dispatch('Outlook.Application')
 cal = outlook.CreateItem(1)
+cal.Display()
 cal.Subject = "PSC-Vorbesprechung"
+cal.MeetingStatus = 1
 cal.Body = "Hello all"
 #cal.Location = "Microsoft Teams Meeting"
-cal.Display()
+
 inspector = outlook.ActiveInspector()
 word_editor = inspector.WordEditor
 word_range = word_editor.Application.ActiveDocument.Content
 word_range.PasteExcelTable(False, False, True)
+
+
+#excel_path = r'C:\Users\HPF1COB\Documents\Anaconda\VS code\Outlook meeting\input.xlsx'
+#excel = win32.gencache.EnsureDispatch('Excel.Application')
+#excel.Visible = False
+#excel.DisplayAlerts = False
+#wb = excel.Workbooks.Open(excel_path)
+#ws = wb.Worksheets(1)
+# ws.Range("A1:L7").Copy()
+# wb.Close()
